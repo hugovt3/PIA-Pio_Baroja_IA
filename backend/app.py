b37abbb8268a2631d712b3bd7611cb35a59ed957
Iker <ikeralvarez21@gmail.com>
2026-01-16 22:35:35 +0100

    Actualización para guardar pdfs en docs, styles, index, y html de subidos

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,58 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request, render_template
 import requests
+import os
 
 app = Flask(
         __name__,
-        static_folder="../frontend", #cambio de template a frontend carpeta
+        static_folder="../frontend", #Carpeta frontend para acceder a styles, script.js e Images
         static_url_path="", #Cambia la busqueda
-        template_folder="../frontend" #cambio de template a frontend carpeta
+        template_folder="../frontend/templates" #cambio de template a frontend/templates carpeta
         )
 
+RUTA_GUARDAR_PDFS_SERVIDOR = os.path.join(os.path.dirname(__file__), "..", "docs")# Ruta absoluta a la carpeta donde se guardarán los PDFs
 
-@app.route("/") # Ruta de prueba La primera ruta que se accede
+
+#--------------------------
+#Pagina para servir el frontend
+#--------------------------
+@app.route("/")
 def home():
     return send_from_directory(app.template_folder, "index.html")
 
+#--------------------------
+#Ruta para subir los archivos PDF a la carpeta doscs
+#-------------------------- 
+@app.route("/upload", methods=["POST"])
+def upload_pdfs():
+    # request.files.getlist permite recibir varios archivos
+    pdf_files = request.files.getlist("pdfs")
+
+    saved_files = []
+
+    for pdf in pdf_files:
+        # Evitamos archivos vacíos
+        if pdf.filename == "":
+            continue
+
+        # Construimos la ruta final donde se guardará el PDF
+        save_path = os.path.join(RUTA_GUARDAR_PDFS_SERVIDOR, pdf.filename)
+
+        # Guardamos el archivo en el servidor
+        pdf.save(save_path)
+
+        saved_files.append(pdf.filename)
+
+    return render_template("upload_success.html", archivos=saved_files)
+
+
+
+
+
+
+
+#--------------------------
+#Pagina prueba de ollama
+#--------------------------
 @app.route("/test-ollama") #test de ollama
 def test_ollama():
     url = "http://localhost:11434/api/generate"
